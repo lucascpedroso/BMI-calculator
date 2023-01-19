4d996389d56b454420c1df45012af90c521b90ca
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-01-19 08:21:10 +0000

    add modules and keydown function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
+import { Modal } from './modal.js'
+
 // variables //
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-
-const Modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    }
-}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -30,9 +19,6 @@ form.onsubmit = event => {
     Modal.open()
 }
 
-Modal.buttonClose.onclick = () => {
-    Modal.close()
-}
 
 function BMI(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
