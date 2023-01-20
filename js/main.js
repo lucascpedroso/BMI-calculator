78f4412de7d62bda9353e4218a0d9b4581e771b6
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-01-20 08:00:48 +0000

    code refactoring

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
+import { calculateBMI, notANumber } from './utils.js'
 
-// variables //
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+form.oninput = () => {
+    AlertError.close()
+}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -13,26 +16,22 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = BMI(weight, height)
+    const result = calculateBMI(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `Your BMI is ${result}`
 
     Modal.message.innerText = message
     Modal.open()
-}
-
-function notANumber(value) {
-    return isNaN(value) || value == ""
-}
-
-function BMI(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
